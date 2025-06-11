3bcb25faa01889b759169bd65576ebf6649911fa
Andy Tong <ajtong@ucsd.edu>
2025-06-11 15:49:17 -0700

    Initial Commit

---
+++ New Papers/New_PapersApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_PapersApp.swift
+//  New Papers
+//
+//  Created by Andy Tong on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct New_PapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New PapersTests/New_PapersTests.swift
@@ -0,0 +1,16 @@
+//
+//  New_PapersTests.swift
+//  New PapersTests
+//
+//  Created by Andy Tong on 6/11/25.
+//
+
+import Testing
+
+struct New_PapersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
